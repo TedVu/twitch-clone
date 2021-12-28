85f6bd9bca7b9e49b40d59cb7854448a325bd2b8
TedEngineer <tedvu184@gmail.com>
2021-12-28 22:13:36 +1100

    Add react router dom and implement a toy example of navigation

--- twitch-clone/src/components/App.js
+++ twitch-clone/src/components/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const PageOne = () => {
+  return <div> Page 1</div>;
+};
+
+const PageTwo = () => {
+  return <div> Page 2</div>;
+};
 
 const App = () => {
-  return <div>App change</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pageTwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
